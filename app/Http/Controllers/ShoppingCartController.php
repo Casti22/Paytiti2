e1896cbe1b8c42a6b407c24a9b17e61bfa62d9e2
Ignacio Andres del Castillo Nagel <adelcastillonagel@gmail.com>
2018-07-27 21:08:30 -0400

    Productos del Carrito con VUE 2

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\ProductsCollection;
 
 class ShoppingCartController extends Controller
 {
@@ -12,5 +13,8 @@ class ShoppingCartController extends Controller
     
     public function show(Request $request){
         return view('shopping_cart.show', ['shopping_cart' => $request->shopping_cart]);
+    }
+    public function products(Request $request){
+        return new ProductsCollection($request->shopping_cart->products()->get());
     }
 }
